5c26bb5a98833648cae26284e2e8d8c7109f3ca4
Rahul Tarak <cryogenicplanet@gmail.com>
2023-05-09 19:24:00 -0700

    🔍 Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,31 @@ import { api } from '~/utils/api'
 import { Toaster } from 'react-hot-toast'
 
 import '~/styles/globals.css'
+import Head from 'next/head'
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Toaster position="top-right" />
       <Component {...pageProps} />
     </>
