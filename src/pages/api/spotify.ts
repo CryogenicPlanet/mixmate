f2b8a88a9ca9068398ca23a2a642ec6871ed5b34
Rahul Tarak <cryogenicplanet@gmail.com>
2023-05-09 19:17:57 -0700

    🔧 Url config

--- src/pages/api/spotify.ts
+++ src/pages/api/spotify.ts
@@ -2,9 +2,13 @@ import { type NextApiRequest, type NextApiResponse } from 'next'
 import SpotifyWebApi from 'spotify-web-api-node'
 import { env } from '~/env.mjs'
 
-export const baseUrl = `http://100.72.180.26:3000`
+export const getBaseUrl = () => {
+  if (typeof window !== 'undefined') return '' // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}` // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}` // dev SSR should use localhost
+}
 
-export const REDIRECT_URI = `${baseUrl}/api/callback`
+export const REDIRECT_URI = `${getBaseUrl()}/api/callback`
 
 export const getSpotify = () => {
   return new SpotifyWebApi({
