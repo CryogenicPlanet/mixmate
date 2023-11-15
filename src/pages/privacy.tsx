8e154b5168914862e8ce09ed7307041cc2072f26
Rahul Tarak <cryogenicplanet@gmail.com>
2023-11-16 02:09:12 +0530

    add privacy policy

---
+++ src/pages/privacy.tsx
@@ -0,0 +1,71 @@
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <h1>Privacy Policy for MixMate by Modfy Inc</h1>
+      <p>
+        <strong>Last Updated: November 16, 2023</strong>
+      </p>
+
+      <h2>Introduction</h2>
+      <p>
+        Welcome to MixMate by Modfy Inc ("MixMate", "we", "us", or "our").
+        MixMate provides a playlist creation service through Spotify. This
+        Privacy Policy explains our practices regarding the collection, use, and
+        disclosure of information that we receive when you use our services.
+      </p>
+
+      <h2>No Data Collection and Storage</h2>
+      <p>
+        MixMate does not collect or store any personal data from its users. All
+        interactions with MixMate are processed in real-time, and no user
+        information is retained after the session ends.
+      </p>
+
+      <h2>Data Processing</h2>
+      <ol>
+        <li>
+          <strong>User Queries</strong>: User queries related to song
+          preferences or playlist types are sent to OpenAI and Spotify for
+          processing. We do not store these queries or any information derived
+          from them.
+        </li>
+        <li>
+          <strong>Third-Party Data Processing</strong>: Since we leverage OpenAI
+          and Spotify for our service, users are subject to the privacy policies
+          of these respective services.
+        </li>
+      </ol>
+
+      <h2>Third-Party Links</h2>
+      <p>
+        Our service may contain links to third-party websites or services (like
+        Spotify) that are not owned or controlled by MixMate. We have no control
+        over, and assume no responsibility for, the content, privacy policies,
+        or practices of any third-party websites or services.
+      </p>
+
+      <h2>Children's Privacy</h2>
+      <p>
+        MixMate does not knowingly collect or solicit personal information from
+        anyone under the age of 13. If we learn that we have collected personal
+        information from a child under 13, we will delete that information as
+        quickly as possible.
+      </p>
+
+      <h2>Changes to Our Privacy Policy</h2>
+      <p>
+        MixMate may modify or update this Privacy Policy from time to time, so
+        please review it periodically. We may provide you with notice of
+        material changes through our service or through other means.
+      </p>
+
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions about this Privacy Policy, please contact us
+        at: <a href="mailto:hey@cryo.wtf">hey@cryo.wtf</a>.
+      </p>
+    </div>
+  )
+}
+
+export default PrivacyPolicy
